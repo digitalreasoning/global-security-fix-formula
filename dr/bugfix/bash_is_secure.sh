d8475cf00cd0dcd826eca0365426ce1833061594
cory.hughes <cory.hughes@digitalreasoning.com>
2014-09-26 15:52:50 -0500

    Added test for CVE-2014-7169

--- dr/bugfix/bash_is_secure.sh
+++ dr/bugfix/bash_is_secure.sh
@@ -5,3 +5,7 @@ which bash >/dev/null || exit 0;
 if [ ! `env x='() { :;}; echo vulnerable' bash -c 'echo ""' 2>/dev/null | grep -c vulnerable` ];
 then exit 1;
 fi;
+
+if [ ! `env X='() { (a)=>\' bash -c "echo hostname" &>/dev/null; cat echo 2>/dev/null | grep -c "$(hostname)"` ];
+then exit 1;
+fi;
